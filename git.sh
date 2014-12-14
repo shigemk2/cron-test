310b69d366487f4f04d57620680b4c22ffee7add
shigemk2 <i.am.shige@gmail.com>
2014-12-14 16:52:01 +0900

    test

--- git.sh
+++ git.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 DIR_PATH=$1
 cd $DIR_PATH
+#!/bin/sh
+ 
+for i in `seq 1 1 1000`
+do
+    touch 1.out
+    git add .
+    git commit -m "test"
+done
+exit 0
 git add .
 git commit -m "test"
 git push origin master
